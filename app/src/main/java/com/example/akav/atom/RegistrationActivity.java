117937ebbd8cc9ebc62b18329237f8fb3f61b89b
vaiditya <mayorvaiditya25@gmail.com>
2017-12-26 19:46:27 +0530

    QR code generation during registration and saving it to gallery for future reference

--- app/src/main/java/com/example/akav/atom/RegistrationActivity.java
+++ app/src/main/java/com/example/akav/atom/RegistrationActivity.java
@@ -1,22 +1,38 @@
 package com.example.akav.atom;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
+import android.os.Environment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import net.glxn.qrgen.android.QRCode;
+import net.glxn.qrgen.core.scheme.VCard;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+import static com.example.akav.atom.R.id.imageView;
 
 public class RegistrationActivity extends AppCompatActivity {
 
     private EditText fullNameEditText;
     private EditText emailEditText;
     private EditText phoneNumberEditText;
+    private EditText uniquepfno;
 
     private Spinner categorySpinner;
 
@@ -38,20 +54,28 @@ public class RegistrationActivity extends AppCompatActivity {
         phoneNumberEditText = (EditText) findViewById(R.id.phone_number);
         categorySpinner = (Spinner) findViewById(R.id.category_spinner);
         gotoLogin = (Button) findViewById(R.id.goto_login_button);
-        registerButton = (Button) findViewById(R.id.register_button);
+        registerButton = (Button) findViewById(R.id.qrcodebutton);
+        uniquepfno=(EditText)findViewById(R.id.pf_number);
+
 
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent gotoHomeIntent = new Intent(RegistrationActivity.this, HomeActivity.class);
+               /*Intent gotoHomeIntent = new Intent(RegistrationActivity.this, HomeActivity.class);
 
                 //Only for Testing
                 fullName = fullNameEditText.getText().toString();
                 Uri nameUri = Uri.parse(fullName);
                 gotoHomeIntent.setData(nameUri);
-                startActivity(gotoHomeIntent);
+                startActivity(gotoHomeIntent);*/
+            alert();
+
+
+
+
             }
         });
+
 
         gotoLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -94,4 +118,75 @@ public class RegistrationActivity extends AppCompatActivity {
             }
         });
     }
+    public void alert(){
+
+        AlertDialog.Builder alertadd = new AlertDialog.Builder(
+                RegistrationActivity.this);
+        alertadd.setTitle("QR Code verification");
+
+
+        LayoutInflater factory = LayoutInflater.from(RegistrationActivity.this);
+        final View view = factory.inflate(R.layout.qrcode, null);
+
+        // ImageView image= (ImageView) view.findViewById(R.id.imageView);
+       // image.setImageResource(R.drawable.logo);
+        VCard abhay=new VCard(fullNameEditText.getText().toString())
+                .setEmail(emailEditText.getText().toString())
+                .setTitle("PFNumber:"+uniquepfno.getText().toString())
+                .setPhoneNumber(phoneNumberEditText.getText().toString())
+                .setAddress(category);
+
+        Bitmap myBitmap= QRCode.from(abhay).bitmap();
+        final ImageView image=(ImageView) view. findViewById(imageView);
+        image.setImageBitmap(myBitmap);
+
+
+
+        alertadd.setView(view);
+        alertadd.setNeutralButton("SAVE", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dlg, int sumthin) {
+                BitmapDrawable draw = (BitmapDrawable)image.getDrawable();
+                Bitmap bitmap = draw.getBitmap();
+
+                FileOutputStream outStream = null;
+                File sdCard = Environment.getExternalStorageDirectory();
+                File dir = new File(sdCard.getAbsolutePath() + "/Atom");
+                dir.mkdirs();
+                String fileName = String.format("qrcode.jpg", System.currentTimeMillis());
+                File outFile = new File(dir, fileName);
+
+                try {
+                    outStream = new FileOutputStream(outFile);
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outStream);
+                    outStream.flush();
+                    outStream.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                intent.setData(Uri.fromFile(outFile));
+                sendBroadcast(intent);
+                savemessage();
+                finish();
+
+            }
+
+        });
+        alertadd.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+
+               dialog.dismiss();
+            } });
+
+
+        alertadd.show();
+
+    }
+    private void savemessage(){
+        Toast.makeText(this, "QRcode saved for verification in future", Toast.LENGTH_SHORT).show();
+    }
 }
+
+
+
