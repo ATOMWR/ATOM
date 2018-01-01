a0606e725e15b224e2635a1ee654a797069bbfe2
vaiditya <mayorvaiditya25@gmail.com>
2018-01-01 19:44:18 +0530

    gridview in overtime

--- app/src/main/java/com/example/akav/atom/OvertimeGridActivity.java
+++ app/src/main/java/com/example/akav/atom/OvertimeGridActivity.java
@@ -3,11 +3,16 @@ package com.example.akav.atom;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OvertimeGridActivity extends AppCompatActivity {
 
     private TextView selectedDate;
+    String st;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +26,31 @@ public class OvertimeGridActivity extends AppCompatActivity {
         String startDate = intent.getStringExtra("startDate");
         String endDate = intent.getStringExtra("endDate");
 
+
         selectedDate.setText(startDate + " TO " + endDate);
 
+        //gridview logic
+        String[] s=new String[28];
+        for(int i=0;i<s.length;i++){
+            s[i]=" "+(i+1)+" ";
+        }
+        GridView gridView=(GridView)findViewById(R.id.gridview);
+        gridView.setAdapter(new TextViewAdapter(this, s));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+
+                 st=parent.getItemAtPosition(position).toString();
+                msg();
+            }
+        });
+
+
+
+    }
+    private void msg(){
+        Toast.makeText(this, st+"date is selected", Toast.LENGTH_SHORT).show();
     }
 }
